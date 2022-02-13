9ce917ed73b180708007b2fe4b94a4ba1d83cdaf
Crowdbotics <team@crowdbotics.com>
2022-02-13 03:53:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "railvantage_33572",
+  url: "https://railvantage_33572.botics.co",
+  api: "https://railvantage_33572.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
